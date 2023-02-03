3794d211ccabf58187df5f48ab4a3be40e38f20c
msjai <103417345+msjai@users.noreply.github.com>
2023-02-04 01:49:14 +0300

    iter1

--- mutex_impl.go
+++ mutex_impl.go
@@ -6,23 +6,23 @@ import (
 
 const mutexLocked = 1
 
-type newMutex struct {
+type extMutex struct {
 	locked int32
 	c      chan struct{}
 }
 
 func New() Mutex {
-	return &newMutex{c: make(chan struct{}, 1)}
+	return &extMutex{c: make(chan struct{}, 1)}
 }
 
-func (nM *newMutex) LockChannel() <-chan struct{} {
+func (nM *extMutex) LockChannel() <-chan struct{} {
 	if atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked) {
 		nM.c <- struct{}{}
 	}
 	return nM.c
 }
 
-func (nM *newMutex) Lock() {
+func (nM *extMutex) Lock() {
 	for {
 		if atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked) {
 			return
@@ -30,6 +30,6 @@ func (nM *newMutex) Lock() {
 	}
 }
 
-func (nM *newMutex) Unlock() {
+func (nM *extMutex) Unlock() {
 	atomic.CompareAndSwapInt32(&nM.locked, mutexLocked, 0)
 }
