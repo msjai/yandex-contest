1f282d1129bdb08e7913f20800c8090096cb9714
msjai <103417345+msjai@users.noreply.github.com>
2023-02-03 23:13:38 +0300

    iter1

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,5 +1,42 @@
 package contest
 
+import (
+	"sync/atomic"
+)
+
+const mutexLocked = 1
+
+type newMutex struct {
+	locked int32
+	c      chan struct{}
+}
+
 func New() Mutex {
-	return ... 
+	return &newMutex{c: make(chan struct{}, 1)}
+}
+
+func (nM *newMutex) LockChannel() <-chan struct{} {
+	if atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked) {
+		nM.c <- struct{}{}
+		return nM.c
+	}
+	return nM.c
+}
+
+func (nM *newMutex) Lock() {
+	if atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked) {
+		return
+	}
+
+	for {
+		if atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked) {
+			break
+		}
+	}
+}
+
+func (nM *newMutex) Unlock() {
+	if atomic.CompareAndSwapInt32(&nM.locked, mutexLocked, 0) {
+		return
+	}
 }
