c9aca0ebc1e2b6691117bddd0b71496a2ffe7a61
msjai <103417345+msjai@users.noreply.github.com>
2023-02-06 15:03:57 +0300

    iter3

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,36 +1,23 @@
 package contest
 
-import (
-	"sync"
-	"sync/atomic"
-)
-
-const mutexLocked = 1
-
 type extMutex struct {
-	locked int32
-	stMu   sync.Mutex
-	c      chan struct{}
+	c chan struct{}
 }
 
 func New() Mutex {
-	return &extMutex{c: make(chan struct{}, 1)}
+	ch := make(chan struct{}, 1)
+	ch <- struct{}{}
+	return &extMutex{c: ch}
 }
 
 func (nM *extMutex) LockChannel() <-chan struct{} {
-	if atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked) {
-		nM.stMu.Lock()
-		nM.c <- struct{}{}
-	}
 	return nM.c
 }
 
 func (nM *extMutex) Lock() {
-	nM.stMu.Lock()
-	atomic.CompareAndSwapInt32(&nM.locked, 0, mutexLocked)
+	<-nM.c
 }
 
 func (nM *extMutex) Unlock() {
-	nM.stMu.Unlock()
-	atomic.CompareAndSwapInt32(&nM.locked, mutexLocked, 0)
+	nM.c <- struct{}{}
 }
