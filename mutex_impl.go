1c162268a1455d1b574cda7f2d3a45dc6d99ad2e
msjai <103417345+msjai@users.noreply.github.com>
2023-02-07 15:46:50 +0300

    Iter3 (#3)
    * iter2
    
    * iter2
    
    * iter2
    
    * iter2
    
    * iter3
    
    * iter3

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,5 +1,23 @@
 package contest
 
+type extMutex struct {
+	c chan struct{}
+}
+
 func New() Mutex {
-	return ... 
+	ch := make(chan struct{}, 1)
+	ch <- struct{}{}
+	return &extMutex{c: ch}
+}
+
+func (nM *extMutex) LockChannel() <-chan struct{} {
+	return nM.c
+}
+
+func (nM *extMutex) Lock() {
+	<-nM.c
+}
+
+func (nM *extMutex) Unlock() {
+	nM.c <- struct{}{}
 }
